d06ab4d9b54947d143911e108264db63431e56fc
Gaidysheff <gaidysheff@mail.ru>
2025-07-31 14:50:40 +0500

    test-1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  // base: process.env.VITE_BASE_PATH || "E-WEBSITE/tree/main/frontend",
+  base: process.env.VITE_BASE_PATH || "E-WEBSITE/tree/main/frontend",
 });
