4f2733e014f1ebd3dbb60754b5cdbbccfaac64d7
Gaidysheff <gaidysheff@mail.ru>
2025-07-31 14:46:31 +0500

    TanStack loaded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,13 @@
-// import Accordion from "./../components/accordion/Accordion.jsx";
-// import BannerBottles from "./../components/banner/BannerBottles.jsx";
-// import BannerCertificate from "./../components/banner/BannerCertificate.jsx";
-// import BannerConsultancy from "./../components/banner/BannerConsultancy.jsx";
-// import { Blogs } from "./../components/blogs/Blogs.jsx";
-// import { Brands } from "./../components/brands/Brands.jsx";
-// import Contacts from "./../components/contacts/Contacts.jsx";
-// import Hero from "./../components/hero/Hero.jsx";
-// import Introduction from "./../components/introduction/Introduction.jsx";
-// import YandexMap from "./../components/map/YandexMap.jsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
 
 function App() {
-  return (
-    <>
-      <h1 class="text-5xl font-bold underline underline"> Hello world! </h1>
-      <h3 class="text-5xl font-bold underline underline"> Hello world! </h3>
-      <h5 class="text-5xl font-bold underline underline"> Hello world! </h5>
-      <p> Hello world! </p>
-      {/*  */}
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "E-WEBSITE/tree/main/frontend"
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  // base: process.env.VITE_BASE_PATH || "E-WEBSITE/tree/main/frontend",
 });
